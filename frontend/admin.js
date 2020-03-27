cae902712fec155d74fabc9871023f1bbafc4cbb
horix7 <horix77777@gmail.com>
2020-03-27 11:08:54 +0200

    admin

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,195 +1,211 @@
 
 let adminPage = document.querySelector('.body')
 let landing = `
-      
-<div class="dash">
-<div class="menu">
-    <img src="./assets/WhatsApp Image 2020-03-24 at 19.55.29 (1).jpeg" alt="">
- <button class="pox">Bids</button>
- <button class="pox">Auctions</button>
- <button class="pox">Users</button>
+
+
+<div class="mennu">
+<img src="./assets/WhatsApp Image 2020-03-24 at 19.55.29 (1).jpeg" alt="" width="30%">
+<h2>Admin DashBoard</h2>
+<button>Logout</button>
 </div>
 
-<div class="info">
-    
- <div class="infos">
-     <div class="big-box">
-         <h4 class="dashnfo">Total Revenue</h4>
-      <h1 class="dashInfo">231000</h1>
-     </div>
-     <div class="big-box">
-      <h4 class="dashnfo">Total Customers</h4>
-   <h1 class="dashInfo">231000</h1>
-  </div>
-  <div class="big-box">
-      <h4 class="dashnfo">New Customers</h4>
-   <h1 class="dashInfo">231000</h1>
-  </div>
-  <div class="big-box">
-     <h4 class="dashnfo">Total Winners</h4>
-  <h1 class="dashInfo">20</h1>
+<div class="infos">
+<div class="big-box">
+    <h5 class="tabble">Total Revenue</h5>
+ <div class="dashInfo">RWF 231000</div>
 </div>
- </div>
-
- <div class="winners">
-     <div class="winnerHead">
-         <div> Winner </div>
-         <div> Phone </div>
-         <div> Bid Tme </div>
-         <div> Winn for </div>
-     </div>
-
-     <div class="winner">
-         <div> Benedict Okolie </div>
-         <div> 019213934 </div>
-         <div> 23:32:12 </div>
-         <div> mercedenz benz  </div>
-     </div>
-
-       <div class="winner">
-         <div> Moise okolie </div>
-         <div> 019213934 </div>
-         <div> 23:32:12 </div>
-         <div> mercedenz benz  </div>
-     </div>
- </div>
-
-<div class="sign">
- <h1>Publish A winner</h1>
-     <div class="grid-two"><input type="text" required placeholder="Winner Name" class="login"> 
-         <input type="text" required placeholder="Won for" class="login"> </div>
- <div class="grid-two">
-     <input type="text" required placeholder="winning time" class="login">
-     <input type="text"  placeholder="Picture link" class="login">
-     </div>
-     <div class="grid-two">
-         <button class="submit" onclick="created()"> Publish Winner</button>
-     </div>
+<div class="big-box">
+ <h5 class="tabble">Total Customers</h5>
+<div class="dashInfo">231000</div>
 </div>
-
-<div class="sign">
- <div class="grid-two">
-     <button class="create" onclick="produi()"> Create An Auction <span>+</span></button>
-    </div>
+<div class="big-box">
+ <h5 class="tabble">New Customers</h5>
+<div class="dashInfo">231000</div>
 </div>
-
-
-<h4 class="sign"> Upcoming Auctions</h4>
-<div class="winners">
- <div class="productHead">
-     <div> Product </div>
-     <div> launching </div>
-     <div> interested </div>
-     <div> Bid Target </div>
-     <div> launch time </div>
-     <div> Actions </div>
-
-
- </div>
-
- <div class="produi">
-     <div> Benedict Okolie </div>
-     <div> 019213934 </div>
-     <div> 23:32:12 </div>
-     <div> mercedenz benz  </div>
-     <div> mercedenz benz  </div>
- <div> <button>cancel Auction</button> <br> <a href="" class="rg">Dowload Data</a></div>
-
-
- </div>
-
-   <div class="produi">
-     <div> Moise okolie </div>
-     <div> mercedenz benz  </div>
-     <div> 019213934 </div>
-     <div> 23:32:12 </div>
-     <div> mercedenz benz  </div>
- <div> <button>cancel Auction</button> <br> <a href="" class="rg">Dowload Data</a></div>
-
- </div>
-</div>
-
-<h4 class="sign"> Current Auctions</h4>
-<div class="winners">
- <div class="productHead">
-     <div> Product </div>
-     <div> bids </div>
-     <div> deadline </div>
-     <div> Revenue </div>
-     <div> date </div>
-     <div> Actions </div>
-
- </div>
-
- <div class="produi">
-     <div> Benedict Okolie </div>
-     <div> 019213934 </div>
-     <div> 23:32:12 </div>
-     <div> mercedenz benz  </div>
-     <div> mercedenz benz  </div>
- <div>  <br> <a href="" class="rg">Dowload Data</a></div>
-
-
- </div>
-
-   <div class="produi">
-     <div> Moise okolie </div>
-     <div> mercedenz benz  </div>
-     <div> 019213934 </div>
-     <div> 23:32:12 </div>
-     <div> mercedenz benz  </div>
- <div>  <br> <a href="" class="rg">Dowload Data</a></div>
-
- </div>
-</div>
-
-
-
-
-
-<h2 class="sign">Customers</h2>
-<div class="grid-two flo"><input type="text" placeholder="Search" > <button>Search</button></div>
-<div class="winners">
-<div class="userHead">
-   <div> Full Name </div>
-   <div> User Name </div>
-   <div> Email </div>
-   <div> Phone Number </div>
-   <div> Age </div>
-   <div> Registered Date  </div>
-   <div> Action  </div>
-
-
-</div>
-
-<div class="useri">
-   <div> Benedict Okolie </div>
-   <div> 019213934 </div>
-   <div> 23:32:12 </div>
-   <div> mercedenz benz  </div>
-   <div> mercedenz benz  </div>
-   <div> mercedenz benz  </div>
- <div> <button>Delete</button> <br> <a href="" class="rg">Dowload Data</a></div>
-
-
-</div>
-
- <div class="useri">
-   <div> Moise okolie </div>
-   <div> mercedenz benz  </div>
-   <div> 019213934 </div>
-   <div> 23:32:12 </div>
-   <div> mercedenz benz  </div>
-   <div> mercedenz benz  </div>
-   <div> <button>Delete</button> <br> <a href="" class="rg">Dowload Data</a></div>
-
+<div class="big-box">
+<h5 class="tabble">Total Winners</h5>
+<div class="dashInfo">20</div>
 </div>
 </div>
 
 
+<div class="wid">
+
+<h5 class="tabble"> Winners</h5>
+
+<div class="table-responsive">
+<table class="table  table-dark">
+    <thead class="thead-red">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">name</th>
+        <th scope="col">username</th>
+        <th scope="col">age</th>
+        <th scope="col">phone Number</th>
+        <th scope="col">email</th>
+        <th scope="col">date Registered</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>smith</td>
+        <td>smansnds</td>
+        <td>34</td>
+        <td>02393409234</td>
+        <td>email@email.com</td>
+        <td>12:31:12</td>
+
+      </tr>
+      <tr>
+        <th scope="row">1</th>
+        <td>smith</td>
+        <td>smansnds</td>
+        <td>34</td>
+        <td>02393409234</td>
+        <td>email@email.com</td>
+        <td>12:31:12</td>
+
+      </tr>
+      <tr>
+        <th scope="row">1</th>
+        <td>smith</td>
+        <td>smansnds</td>
+        <td>34</td>
+        <td>02393409234</td>
+        <td>email@email.com</td>
+        <td>12:31:12</td>
+
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+<div class="llil">
+
+<div >
+<button class="submit" onclick="createWin()"> Publish Winner</button>
+<button class="submit" onclick="produi()"> Create An Auction</button>
 
 </div>
+
+
+<h5 class="tabble"> Upcoming Auctions</h5>
 </div>
+
+<div class="table-responsive">
+<table class="table table-bordered table-striped">
+    <thead class="thead-red">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">product</th>
+        <th scope="col">interested</th>
+        <th scope="col">launch time</th>
+        <th scope="col">target</th>
+        <th scope="col">launch date</th>
+        <th scope="col">Action</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Iphone 30 max</td>
+        <td>231</td>
+        <td>12:31:12</td>
+        <td>321</td>
+        <td>12:31:12</td>
+        <td><a href="">cancel Acution</a></td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Iphone 30 max</td>
+        <td>123</td>
+        <td>12:31:12</td>
+        <td>321</td>
+        <td>12:31:12</td>
+        <td><a href="">cancel Acution</a></td>
+
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Iphone 30 max</td>
+        <td>123</td>
+        <td>12:31:12</td>
+        <td>321</td>
+        <td>12:31:12</td>
+        <td><a href="">cancel Acution</a></td>
+
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+<h5 class="tabble"> Current Auctions</h5>
+
+<div class="table-responsive">
+<table class="table table-bordered table-striped">
+    <thead class="thead-red">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">product</th>
+        <th scope="col">bids</th>
+        <th scope="col">dead line</th>
+        <th scope="col">revenue</th>
+        <th scope="col"> date</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Iphone 30 max</td>
+        <td>231</td>
+        <td>12:31:12</td>
+        <td>321</td>
+        <td>12:31:12</td>
+
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Iphone 30 max</td>
+        <td>123</td>
+        <td>12:31:12</td>
+        <td>321</td>
+        <td>12:31:12</td>
+
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Iphone 30 max</td>
+        <td>123</td>
+        <td>12:31:12</td>
+        <td>321</td>
+        <td>12:31:12</td>
+
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+<h4 class="leftter">
+Dowload Data
+</h4>
+
+<ul class="liist">
+<li><a href="">All Users Data</a></li>
+<li><a href="">All product Data</a></li>
+<li><a href="">All bids Data</a></li>
+<li><input type="number" placeholder="Id " class="soso"> <a href="">One Auction Data </a></li>
+
+</ul>
+
+
+
 `
 
 let createProd = `
@@ -200,22 +216,54 @@ let createProd = `
 </div>
 <div class="sign-in">
     <h1>Create Product </h1>
-    <input type="text" required placeholder="Produc Name" class="login"> 
-    <input type="number" required placeholder="Bid Target" class="login">
+    <input type="text" required placeholder="Produc Name" class="inputsAdmin"> 
+    
     <div class="grid-two">
-    <div><div class="mtn2">Launching Date </div>
-    <input type="date" required  class="login left"></div>
-    <div><div class="mtn2 wes">Ending Date </div>
-    <input type="date" required  class="login rg wes"> </div>
+    <input type="number" required placeholder="Bid Target" class="inputsAdmin">
+    <input type="number" required placeholder=" winners" class="inputsAdmin">
     </div>
-    <div class="grid-two"><input type="number" required placeholder="Price" class="login"> </div>
-    <input type="text" required placeholder="Image Link" class="login"> 
-    <button class="submit" onclick="loginToproducts()">Submit</button>
+    <div class="grid-two">
+
+    <div><div class="mtn2">Launching Date </div>
+    <input type="date" required  class="inputsAdmin left"></div>
+    <div><div class="mtn2 wes">Ending Date </div>
+    <input type="date" required  class="inputsAdmin rg wes"> </div>
+    </div>
+
+    <div><div class="mtn2">Starting Hour </div>
+    <input type="time" required  class="inputsAdmin left"></div>
+    <div class="grid-two"><input type="number" required placeholder="Price" class="inputsAdmin"> </div>
+    <input type="text" required placeholder="Image Link" class="inputsAdmin"> 
+    <button class="submit" onclick="inputsAdminToproducts()">Submit</button>
    </div>
 </div>
 `
 adminPage.innerHTML = landing
 
+let createWinner = `
+ <div class="topper">
+       
+<div class="hold">
+<div class="Close" onclick="admin()">+</div>
+</div>
+<div class="sign">
+<h1>Publish A winner</h1>
+<div class="grid-two"><input type="text" required placeholder="Winner Name" class="inputsAdmin"> 
+    <input type="text" required placeholder="Won for" class="inputsAdmin"> </div>
+<div class="grid-two">
+<input type="text" required placeholder="winning time" class="inputsAdmin">
+<input type="text"  placeholder="Picture link" class="inputsAdmin">
+</div>
+<div class="grid-two">
+    <button class="submit" onclick="created()"> Publish Winner</button>
+</div>
+</div>
+ </div>
+
+`
+let createWin = () => {
+    adminPage.innerHTML = createWinner
+}
 let admin = () => {
     adminPage.innerHTML = landing
 
