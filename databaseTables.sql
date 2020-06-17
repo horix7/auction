5c9b087e1eeb3e5eeeebdced73b554d362204cf5
dushime J.pierre <horix77777@gmail.com>
2020-06-17 12:14:59 -0700

    newUpdate12

--- databaseTables.sql
+++ databaseTables.sql
@@ -1,128 +1,142 @@
-CREATE TABLE users(
-id SERIAL  PRIMARY KEY ,
-firstname VARCHAR(255),
-secondname VARCHAR(255),
-email VARCHAR(255),
-phone VARCHAR(255),
-password VARCHAR(255),
-address VARCHAR(255),
-isadmin VARCHAR(255),
-gender VARCHAR(255),
-age INT,
-vendor VARCHAR(255),
-date VARCHAR(255)
 
-);
 
-CREATE TABLE vendreq(
-id SERIAL  PRIMARY KEY ,
-sells VARCHAR(255),
-address VARCHAR(255),
-store VARCHAR(255),
-account VARCHAR(255),
-phone VARCHAR(255),
-email VARCHAR(255)
-
+CREATE TABLE bidata (
+    id SERIAL,
+    name varchar(255),
+    username varchar(8000),
+    email varchar(255),
+    product varchar(255),
+    "time" varchar(255),
+    payment varchar(255),
+    revenue varchar(255),
+    fortunes varchar(255),
+    phone varchar(255)
 );
 
 
-CREATE TABLE products(
-id SERIAL  PRIMARY KEY ,
-name VARCHAR(255),
-price VARCHAR(255),
-target VARCHAR(255),
-tickets VARCHAR(255),
-winners VARCHAR(255),
-picture VARCHAR(8000),
-date VARCHAR(255),
-hour VARCHAR(255),
-current VARCHAR(8000),
-sold VARCHAR(8000),
-status VARCHAR(255),
-type VARCHAR(255)
-
-
-);
-
-CREATE TABLE productreq(
-id SERIAL  PRIMARY KEY ,
-name VARCHAR(255),
-price VARCHAR(255),
-target VARCHAR(255),
-winners VARCHAR(255),
-picture VARCHAR(8000),
-date VARCHAR(255),
-hour VARCHAR(255),
-type VARCHAR(255)
-
+CREATE TABLE bids (
+    id SERIAL,
+    product varchar(255),
+    bids varchar(8000),
+    madeby varchar(255),
+    revenue varchar(255),
+    "time" varchar(255)
 );
 
 
-CREATE TABLE wins(
-id SERIAL  PRIMARY KEY ,
-name VARCHAR(255),
-quote VARCHAR(255),
-location  VARCHAR(255),
-product VARCHAR(255),
-price VARCHAR(255),
-picture VARCHAR(8000),
-video VARCHAR(255),
-audion VARCHAR(255)
-
-);
-
-
-CREATE TABLE bids(
-id SERIAL  PRIMARY KEY ,
-product VARCHAR(255),
-bids VARCHAR(8000),
-madeby VARCHAR(255),
-revenue VARCHAR(255),
-time VARCHAR(255)
-
-);
-
-
-CREATE TABLE bidata(
-id SERIAL  PRIMARY KEY ,
-name VARCHAR(255),
-username VARCHAR(255),
-email VARCHAR(255),
-product VARCHAR(255),
-time VARCHAR(255),
-payment VARCHAR(255),
-revenue VARCHAR(255),
-fortunes VARCHAR(255)
-
+CREATE TABLE failed (
+    id SERIAL,
+    data varchar(255)
 );
 
 
 
-CREATE TABLE winners(
-id SERIAL  PRIMARY KEY ,
-name VARCHAR(255),
-username VARCHAR(255),
-age VARCHAR(255),
-email  VARCHAR(255),
-product  VARCHAR(255),
-fortune  VARCHAR(255),
-date  VARCHAR(255)
-
-);
-
-
-CREATE TABLE vendPro(
-id SERIAL  PRIMARY KEY ,
-name VARCHAR(255),
-store VARCHAR(255),
-winners VARCHAR(255),
-hour  VARCHAR(255),
-date  VARCHAR(255),
-price  VARCHAR(255),
-image VARCHAR(2000)
+CREATE TABLE nowdata (
+    paymomo varchar(1000),
+    date varchar(255),
+    id integer NOT NULL
 );
 
 
 
- alter table users add column country-code varchar(255);
-  alter table users add column country varchar(255);
+CREATE TABLE productreq (
+    id SERIAL,
+    name varchar(255),
+    price varchar(255),
+    target varchar(255),
+    winners varchar(255),
+    picture varchar(8000),
+    date varchar(255),
+    hour varchar(255),
+    type varchar(255)
+);
+
+
+CREATE TABLE products (
+    id SERIAL,
+    name varchar(255),
+    price varchar(255),
+    target varchar(255),
+    tickets varchar(255),
+    winners varchar(255),
+    picture varchar(8000),
+    date varchar(255),
+    hour varchar(255),
+    current varchar(8000),
+    sold varchar(8000),
+    status varchar(255),
+    type varchar(255),
+    vendor varchar(255),
+    selling varchar(255)
+);
+
+
+CREATE TABLE users (
+    id SERIAL,
+    firstname varchar(255),
+    secondname varchar(255),
+    email varchar(255),
+    phone varchar(255),
+    password varchar(255),
+    address varchar(255),
+    isadmin varchar(255),
+    gender varchar(255),
+    age integer,
+    vendor varchar(255),
+    date varchar(255),
+    picture varchar(1000),
+    country varchar(255),
+    countrycode varchar(255),
+    store varchar(255)
+);
+
+
+
+CREATE TABLE vendpro (
+    id SERIAL,
+    name varchar(255),
+    store varchar(255),
+    winners varchar(255),
+    hour varchar(255),
+    date varchar(255),
+    price varchar(255),
+    picture varchar(2000)
+);
+
+
+CREATE TABLE vendreq (
+    id SERIAL,
+    sells varchar(255),
+    address varchar(255),
+    store varchar(255),
+    account varchar(255),
+    phone varchar(255),
+    email varchar(255),
+    verified varchar(255)
+);
+
+
+CREATE TABLE winners (
+    id SERIAL,
+    name varchar(255),
+    username varchar(255),
+    age varchar(255),
+    email varchar(255),
+    product varchar(255),
+    fortune varchar(255),
+    date varchar(255)
+);
+
+
+CREATE TABLE wins (
+    id SERIAL,
+    name varchar(255),
+    quote varchar(255),
+    location varchar(255),
+    product varchar(255),
+    price varchar(255),
+    picture varchar(8000),
+    video varchar(255),
+    audion varchar(255)
+);
+
